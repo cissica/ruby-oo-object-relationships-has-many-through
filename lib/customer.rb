7beea98c04c8d1fc25c90d96df80594633b2e908
cissica <cecemartinez@hotmail.com>
2020-11-04 18:47:57 -0800

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require "pry"
+
 class Customer
-  
+    attr_accessor :name, :age 
+    @@all = []
+    
+    def initialize(name, age)
+        @name = name 
+        @age = age 
+        save 
+    end 
+
+    def save 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end 
+
+    def new_meal(waiter, total, tip)
+        customer_meal = Meal.new(waiter, self, total, tip)
+        
+    end 
+
+    def meals
+        Meal.all.select {|m| m.customer == self}
+    end 
+
+    def waiters 
+        meals.collect {|m| m.waiter}
+    end 
 end
